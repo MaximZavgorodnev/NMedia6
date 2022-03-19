8a63a9775e30cc9d148cfb61e0056494d58de24b
MaximZavgorodnev <maxpek9@yandex.ru>
2022-03-19 11:27:22 +0300

    Основное зедание готово

--- app/src/main/java/ru/netology/nmedia/error/AppError.kt
+++ app/src/main/java/ru/netology/nmedia/error/AppError.kt
@@ -1,6 +1,9 @@
 package ru.netology.nmedia.error
 
+import android.provider.Settings.Global.getString
+import ru.netology.nmedia.R
+
 sealed class AppError(var code: String): RuntimeException()
 class ApiError(val status: Int, code: String): AppError(code)
-object NetworkError : AppError("error_network")
-object UnknownError: AppError("error_unknown")
+object NetworkError : AppError((R.string.error_network).toString())
+object UnknownError: AppError((R.string.error_unknown).toString())
