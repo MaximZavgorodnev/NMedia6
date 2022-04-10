431a7108a89d926ae76f62bc91a8d2ddc87a0be2
MaximZavgorodnev <maxpek9@yandex.ru>
2022-04-10 19:25:44 +0300

    1.3 Architecture Components. Начал работу

--- app/src/main/java/ru/netology/nmedia/api/ApiService.kt
+++ app/src/main/java/ru/netology/nmedia/api/ApiService.kt
@@ -50,4 +50,10 @@ interface ApiService {
         @Path("id") id: Long,
         @Query("count") count: Int
     ): Response<List<Post>>
+
+    @GET("posts/{id}/before")
+    suspend fun getAfter(
+        @Path("id") id: Long,
+        @Query("count") count: Int
+    ): Response<List<Post>>
 }
